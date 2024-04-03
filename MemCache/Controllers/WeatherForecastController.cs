07eb030e84c2fba94897f8b3c047c9d9955fa03d
Abay Toguzbayev <abai2010@bk.ru>
2024-04-03 06:36:31 +0500

    Add project files.

---
+++ MemCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MemCache.Controllers
+{
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpPost("SetCache")]
+        public IActionResult SetCache()
+        {
+            _memoryCache.Set("key1", "value1");
+            return Ok();
+        }
+
+        [HttpPost("GetCache")]
+        public IActionResult GetCache()
+        {
+            var found = _memoryCache.TryGetValue("key1", out var value);
+            return Ok(found ? value : "");
+        }
+    }
+}
